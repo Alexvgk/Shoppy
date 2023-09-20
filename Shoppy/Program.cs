c4a9d16979fd6224ee93bdb21ba12dd5c12d78e3
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-09-20 20:59:09 +0300

    made little changes for work with DB

--- Shoppy/Program.cs
+++ Shoppy/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Shoppy.Data;
+using Microsoft.SqlServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ShoppyDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -9,7 +16,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
