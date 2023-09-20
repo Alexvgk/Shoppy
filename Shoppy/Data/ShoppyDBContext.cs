4a3bed1665560399384afb1cc4284f904bf1aac5
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-09-20 20:56:25 +0300

    added DbContext for MSSQL

---
+++ Shoppy/Data/ShoppyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shoppy.Models;
+
+namespace Shoppy.Data
+{
+    public class ShoppyDBContext : DbContext
+    {
+
+        public ShoppyDBContext( DbContextOptions<ShoppyDBContext> options) : base (options) 
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
