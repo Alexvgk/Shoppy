a15857b20e7f4f4407e99983dfbad373a4c81584
Alex Prokopchyk <alexi9prokopchyk@mail.ru>
2023-09-21 21:12:11 +0300

    added controller for category

---
+++ Shoppy/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoppy.Data;
+using Shoppy.Models;
+
+namespace Shoppy.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ShoppyDBContext _db;
+
+        public CategoryController(ShoppyDBContext dBContext)
+        {
+            _db = dBContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
